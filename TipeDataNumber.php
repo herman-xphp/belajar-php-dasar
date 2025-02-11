0f32de07f906541845510f3fb7ca17e9759dab7c
herman-xphp <hermanxphp@gmail.com>
2025-02-11 09:55:09 +0800

    Tipe Data Number

---
+++ TipeDataNumber.php
@@ -0,0 +1,31 @@
+<?php
+
+echo "Decimal: ";
+var_dump(1232);
+
+echo "Octal: ";
+var_dump(03432);
+
+echo "Hexadecimal: ";
+var_dump(0x24242);
+
+echo "Binary: ";
+var_dump(0b1010101010);
+
+echo "Underscore in number: ";
+var_dump(0b1010_1010_1010);
+
+echo "Floating Point: ";
+var_dump(1.782);
+
+echo "Floating Point dengan E notation plus: ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E notation minus: ";
+var_dump(1.7e-3);
+
+echo "Underscore in Floating Point: ";
+var_dump(0.7_82);
+
+echo "Integer Overflow: ";
+var_dump(9223372036854775808);
